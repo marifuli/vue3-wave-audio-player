17bfc974ee569e00177ff8176e92c043e6cbce87
Md Ariful Islam <imdarif122@gmail.com>
2022-07-21 02:56:47 +0600

    Publish

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue3WaveAudioPlayer from './components/Vue3WaveAudioPlayer.vue'
 
-createApp(App).mount('#app')
+Vue3WaveAudioPlayer.install = (Vue) => {
+  Vue.component(Vue3WaveAudioPlayer.name, Vue3WaveAudioPlayer)
+}
+
+export default Vue3WaveAudioPlayer
