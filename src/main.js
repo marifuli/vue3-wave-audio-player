6819ee697d131a08c22b3be5c9969820afe29db1
Md Ariful Islam <imdarif122@gmail.com>
2022-07-22 20:24:28 +0600

    Add vue test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue3WaveAudioPlayer from './components/Vue3WaveAudioPlayer.vue'
+// import Vue3WaveAudioPlayer from './components/Vue3WaveAudioPlayer.vue'
 
-Vue3WaveAudioPlayer.install = (Vue) => {
-  Vue.component(Vue3WaveAudioPlayer.name, Vue3WaveAudioPlayer)
-}
+// Vue3WaveAudioPlayer.install = (Vue) => {
+//   Vue.component(Vue3WaveAudioPlayer.name, Vue3WaveAudioPlayer)
+// }
 
-export default Vue3WaveAudioPlayer
+// export default Vue3WaveAudioPlayer
+import { createApp } from "vue"
+import App from "./App.vue"
+createApp(App).mount("#app")
